8f04f15acb7c56a774437f73b63fc30c792b4b5d
Tausif <tausifmdansari1@gmail.com>
2024-02-17 05:22:16 +0530

    dashboard_cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,17 +17,44 @@ export default async function Dashboard() {
   console.log(data);
 
   return (
-    <div className="flex justify-between mt-6">
-      <div>
-        <button className={"border-2 px-8 py-2 rounded-md "}>
-          Start Writing
-        </button>
+    <div>
+      <div className="flex justify-between mt-6 ml-4 mr-4 rounded-md bg-gray-900 px-10 py-6">
+        <div className={"flex"}>
+          <span className={"text-3xl font-thin"}>Script</span>
+          <span className={"flex font-serif italic text-4xl"}>R.</span>
+        </div>
+
+        <div className={"flex ml-auto"}>
+          <div className={"flex "}>
+            <button
+              className={
+                "border-2 px-8 py-2 mt-6 ml-auto mr-10 rounded-md hover:border-b-emerald-400 hover:scale-105 duration-200 ease-in-out"
+              }
+            >
+              <Link href={"scripts/new"}>New Script</Link>
+            </button>
+          </div>
+          <div className="border-2 px-2 py-2 rounded-md bg-gray-100 text-black hover:bg-gray-300 mr-4 mt-6 duration-200 ease-in-out hover:scale-90">
+            <Logout />
+          </div>
+        </div>
       </div>
-      <div className="border-2 px-2 py-2 rounded-md bg-gray-100 text-black hover:bg-gray-300 mr-7">
-        <Logout />
+      <div className={"flex px-20 py-4"}>
+        <p className={"text-3xl"}> Edits </p>
       </div>
-      <br />
-      <Link href={"scripts/new"}>New Script</Link>
+
+      <div className="card w-full max-w-sm m-auto rounded-lg shadow-md h-auto overflow-hidden">
+        <div className="card-header bg-gray-200 opacity-50 p-4">
+          <h1 className="text-2xl font-bold">Date</h1>
+          <h1 className="text-2xl font-bold">TIme</h1>
+        </div>
+        <div className="card-body p-6 bg-amber-400">
+          <p className="text-gray-700 mb-4">Description</p>
+        </div>
+        <div className="card-footer bg-blue-600 opacity-80 p-4">
+          <button className="button">Title</button>
+        </div>
+      </div>
     </div>
   );
 }
