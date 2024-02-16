1392701995cd7c2b4d3469558b23af46a93c8d08
Tausif <tausifmdansari1@gmail.com>
2024-02-17 02:17:00 +0530

    Dash-Board Logout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,11 @@
 import Logout from "@/components/Logout";
 
 export default function Dashboard() {
-  return <Logout />;
+  return (
+    <div className="grid justify-items-end mr-8 mt-6">
+      <div className="border-2 px-2 py-2 rounded-md bg-gray-100 text-black hover:bg-gray-300">
+        <Logout />
+      </div>
+    </div>
+  );
 }
