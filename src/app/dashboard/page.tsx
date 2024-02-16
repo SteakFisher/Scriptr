7ac2ec7b708fae853430fd912a268905ff75b25d
Tausif <tausifmdansari1@gmail.com>
2024-02-17 02:53:23 +0530

    nothing MAjor
    just felxbox

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,13 @@ import Logout from "@/components/Logout";
 
 export default function Dashboard() {
   return (
-    <div className="grid justify-items-end mr-8 mt-6">
-      <div className="border-2 px-2 py-2 rounded-md bg-gray-100 text-black hover:bg-gray-300">
+    <div className="flex justify-between mt-6">
+      <div>
+        <button className={"border-2 px-8 py-2 rounded-md "}>
+          Start Writing
+        </button>
+      </div>
+      <div className="border-2 px-2 py-2 rounded-md bg-gray-100 text-black hover:bg-gray-300 mr-7">
         <Logout />
       </div>
     </div>
